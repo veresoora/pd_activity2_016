35f315adeddae83a521af47ce99fe0a0da894499
veresoora <lahanabira@gmail.com>
2023-03-22 21:33:09 +0700

    membuat looping

--- switchcase/switchcase.cpp
+++ switchcase/switchcase.cpp
@@ -18,43 +18,45 @@ int main() {
 	float panjang, lebar, tinggi, jejari, alas;
 	int pilihan;
 
-	cout << "---------------------" << endl;
-	cout << "-------M E N U-------" << endl;
-	cout << "---------------------" << endl;
+	do {
 
-	cout << "1. luas persegipanjang" << endl;
-	cout << "2. luas segitiga" << endl;
-	cout << "3. luas lingkaran" << endl;
-	cout << "4. exit" << endl;
+		cout << "---------------------" << endl;
+		cout << "-------M E N U-------" << endl;
+		cout << "---------------------" << endl;
 
-	cout << "masukan pilihan : ";
-	cin >> pilihan;
+		cout << "1. luas persegipanjang" << endl;
+		cout << "2. luas segitiga" << endl;
+		cout << "3. luas lingkaran" << endl;
+		cout << "4. exit" << endl;
 
-	switch (pilihan) {
-	case 1:
-		cout << "\nMasukan Panjangnya = ";
-		cin >> panjang;
-		cout << "\nMasukan Lebarnya   = ";
-		cin >> lebar;
-		cout << "\nMaka Luasnya adalah = " << Luaspersegipanjang(panjang, lebar) << endl;
-		break;
-	case 2:
-		cout << "\nMasukan alasnya   = ";
-		cin >> alas;
-		cout << "\nMasukan tingginya = ";
-		cin >> tinggi;
-		cout << "\nMaka Luasnya adalah = " << luassegitiga(alas, tinggi) << endl;
-		break;
-	case 3:
-		cout << "\nMasukan Jari - Jarinya = ";
-		cin >> jejari;
-		cout << "\nMaka Luasnya adalah = " << luaslingkaran(jejari) << endl;
-		break;
-	case 4:
-		break;
-	default:
-		cout << "\nPilihan yang anda masukan salah !";
-		break;
-	}
+		cout << "masukan pilihan : ";
+		cin >> pilihan;
 
+		switch (pilihan) {
+		case 1:
+			cout << "\nMasukan Panjangnya = ";
+			cin >> panjang;
+			cout << "\nMasukan Lebarnya   = ";
+			cin >> lebar;
+			cout << "\nMaka Luasnya adalah = " << Luaspersegipanjang(panjang, lebar) << endl;
+			break;
+		case 2:
+			cout << "\nMasukan alasnya   = ";
+			cin >> alas;
+			cout << "\nMasukan tingginya = ";
+			cin >> tinggi;
+			cout << "\nMaka Luasnya adalah = " << luassegitiga(alas, tinggi) << endl;
+			break;
+		case 3:
+			cout << "\nMasukan Jari - Jarinya = ";
+			cin >> jejari;
+			cout << "\nMaka Luasnya adalah = " << luaslingkaran(jejari) << endl;
+			break;
+		case 4:
+			break;
+		default:
+			cout << "\nPilihan yang anda masukan salah !";
+			break;
+		}
+	} while (pilihan != 4);
 }
